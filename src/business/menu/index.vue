9d49cfe117a69490bda75a7a7629f1bfeb7a5120
周昊 <zhhsky1030@163.com>
2021-01-13 23:05:07 +0800

    项目中导入ant-design-vue

--- src/business/menu/index.vue
+++ src/business/menu/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <div v-for="menuItem in menuList" v-bind:key="menuItem">
+    <div v-for="(menuItem,key) in menuList" v-bind:key="key">
       <div>
         <a :href="menuItem.path">{{menuItem.label}}</a>
-        <div v-for="menuLiItem in menuItem.children" v-bind:key="menuLiItem" v-if="menuItem.children">
+        <div v-for="(menuLiItem,index) in menuItem.children" v-bind:key="index" v-if="menuItem.children">
           <a :href="menuLiItem.path">{{menuLiItem.label}}</a>
         </div>
       </div>
--- src/demo/AFirstDemo/index.vue
+++ src/demo/AFirstDemo/index.vue
@@ -1,30 +1,36 @@
 <template>
   <div>
-    {{text}}
+    {{ text }}
     <div>
       <p>if 语法块区域</p>
       <p v-if="ifElse">此时属性值为true</p>
       <p v-else>此时属性值为false</p>
-      <button v-on:click="changeIfElse">点一下 就切换</button>
+      <a-button type="primary" v-on:click="changeIfElse">点一下 就切换></a-button>
     </div>
     <div>
       <p>for 语法块区域</p>
-      <ul v-for="item in detail" v-bind:key="item">
+      <ul v-for="(item,i) in detail" v-bind:key="i">
         <li>{{ item.sex }}</li>
         <li>{{ item.like }}</li>
         <li>{{ item.age }}</li>
       </ul>
     </div>
     <div>
-      <input v-model="formData.message" placeholder="edit me">
+      <a-input placeholder="edit me" v-model="formData.message"/>
       <p>Message is: {{ formData.message }}</p>
     </div>
   </div>
 </template>
 
 <script>
+  import { Button, Input } from "ant-design-vue";
+
   export default {
     name: "AFirstDemo",
+    components: {
+      AButton: Button,
+      AInput: Input
+    },
     data () {
       return {
         text: "这是我的第一个vue实例",
